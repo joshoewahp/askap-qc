def39b9f63854057f0f10b766ec81be03a8e4eb4
Joshua Pritchard <joshuapritchard89@gmail.com>
2021-08-16 10:26:39 +1000

    Cleaned up some astrometry.py options

--- astrometry.py
+++ astrometry.py
@@ -11,7 +11,6 @@ from logger import setupLogger
 
 COLORS = ['darkgreen', 'cornflowerblue', 'mediumvioletred', 'mediumturquoise', 'darkorange', 'fuchsia',
           'mediumseagreen', 'rebeccapurple', 'teal', 'darkolivegreen', 'palegreen', 'gold']
-MARKERS = ['d'] * len(COLORS)
 
 logger = logging.getLogger(__name__)
 
@@ -28,10 +27,11 @@ logger = logging.getLogger(__name__)
               help='Region numbers to include (e.g. -r 3 4)')
 @click.option('-s', '--survey', default='icrf', type=click.Choice(['icrf', 'ref', 'racs', 'sumss', 'nvss']),
               help='Name of survey for astrometry comparison')
+@click.option('-S', '--save', type=click.Path(), default=None)
 @click.option('-v', "--verbose", is_flag=True, default=False,
               help="Enable verbose logging.")
 @click.argument('matchdir', type=click.Path())
-def main(axlim, alpha, bins, matchdir, snrlim, regions, survey, verbose):
+def main(axlim, alpha, bins, matchdir, snrlim, regions, survey, save, verbose):
 
     setupLogger(verbose)
 
@@ -41,43 +41,44 @@ def main(axlim, alpha, bins, matchdir, snrlim, regions, survey, verbose):
 
     fig = plt.figure(figsize=(8, 8))
     gs = GridSpec(4, 4, figure=fig)
-    ax1 = fig.add_subplot(gs[1:, :3]) # offsets
-    ax2 = fig.add_subplot(gs[0, :3]) # ra hist
-    ax3 = fig.add_subplot(gs[1:, 3]) # dec hist
+    offset_ax = fig.add_subplot(gs[1:, :3])
+    ra_ax = fig.add_subplot(gs[0, :3])
+    dec_ax = fig.add_subplot(gs[1:, 3])
 
     if not bins:
         bins = 250 if survey == 'racs' else 30
     if not alpha:
         alpha = 0.03 if survey == 'racs' else 0.3
 
-    ax1.set_xlabel('RA Offset (arcsec)')
-    ax1.set_ylabel('Dec Offset (arcsec)')
-    ax1.set_xlim([-axlim, axlim])
-    ax1.set_ylim([-axlim, axlim])
+    offset_ax.set_xlabel('RA Offset (arcsec)')
+    offset_ax.set_ylabel('Dec Offset (arcsec)')
+    offset_ax.set_xlim([-axlim, axlim])
+    offset_ax.set_ylim([-axlim, axlim])
 
-    ax2.set_ylabel('Count')
-    ax3.set_xlabel('Count')
+    ra_ax.set_ylabel('Count')
+    dec_ax.set_xlabel('Count')
 
-    # Plot pixel box, unpack line from returned tuple for legend
-    pixbox, = ax1.plot([-1.25, -1.25, 1.25, 1.25, -1.25],
-                       [-1.25, 1.25, 1.25, -1.25, -1.25],
-                       ls='--',
-                       lw=2,
-                       alpha=1,
-                       zorder=4,
-                       color='gray')
+    # Plot pixel box, unpack Line2D object from returned tuple for legend
+    pixbox, = offset_ax.plot([-1.25, -1.25, 1.25, 1.25, -1.25],
+                             [-1.25, 1.25, 1.25, -1.25, -1.25],
+                             ls='--',
+                             lw=2,
+                             alpha=1,
+                             zorder=4,
+                             color='gray')
 
     # Plot the gridlines
     for i in range(-axlim, axlim+1):
-        ax1.axvline(i, color='k', alpha=0.1, zorder=1)
-        ax1.axhline(i, color='k', alpha=0.1, zorder=1)
+        offset_ax.axvline(i, color='k', alpha=0.1, zorder=1)
+        offset_ax.axhline(i, color='k', alpha=0.1, zorder=1)
     
+
     all_epochs = []
     epochs = [e for e in os.listdir(matchdir) if '00' not in e]
 
     handles = []
     labels = []
-    for color, marker, epoch in zip(COLORS, MARKERS, epochs):
+    for color, epoch in zip(COLORS, epochs):
 
         files = glob.glob(f'{matchdir}/{epoch}/*{survey}-askap.csv')
         if regions:
@@ -92,9 +93,9 @@ def main(axlim, alpha, bins, matchdir, snrlim, regions, survey, verbose):
         
         all_epochs.append(offsets)
 
-        sc = ax1.scatter(offsets.ra_offset.median(), offsets.dec_offset.median(),
-                         marker=marker, s=100, color=color, zorder=10, label=epoch)
-        ax1.scatter(offsets.ra_offset, offsets.dec_offset, s=5, alpha=alpha,
+        sc = offset_ax.scatter(offsets.ra_offset.median(), offsets.dec_offset.median(),
+                         marker='d', s=100, color=color, zorder=10, label=epoch)
+        offset_ax.scatter(offsets.ra_offset, offsets.dec_offset, s=5, alpha=alpha,
                     zorder=3, color='k')
 
         handles.append(sc)
@@ -109,24 +110,24 @@ def main(axlim, alpha, bins, matchdir, snrlim, regions, survey, verbose):
     std_ra = all_epochs.ra_offset.std()
     std_dec = all_epochs.dec_offset.std()
     
-    ax2.hist(all_epochs.ra_offset, bins=bins, histtype='step', color='k')
-    ax3.hist(all_epochs.dec_offset, bins=bins, histtype='step', color='k',
+    ra_ax.hist(all_epochs.ra_offset, bins=bins, histtype='step', color='k')
+    dec_ax.hist(all_epochs.dec_offset, bins=bins, histtype='step', color='k',
              orientation='horizontal')
 
-    medline = ax1.axvline(med_ra, ls='-', zorder=5, color='r')
-    ax1.axvline(med_ra, ls='-', zorder=5, color='r')
-    ax1.axhline(med_dec, ls='-', zorder=5, color='r')
-    ax2.axvline(med_ra, ls='-', zorder=5, color='r')
-    ax3.axhline(med_dec, ls='-', zorder=5, color='r')
+    medline = offset_ax.axvline(med_ra, ls='-', zorder=5, color='r')
+    offset_ax.axvline(med_ra, ls='-', zorder=5, color='r')
+    offset_ax.axhline(med_dec, ls='-', zorder=5, color='r')
+    ra_ax.axvline(med_ra, ls='-', zorder=5, color='r')
+    dec_ax.axhline(med_dec, ls='-', zorder=5, color='r')
 
-    stdline = ax1.axvline(med_ra + std_ra, ls=':', alpha=0.5, zorder=5, color='r')
-    ax1.axhline(med_dec + std_dec, ls=':', alpha=0.5, zorder=5, color='r')
-    ax2.axvline(med_ra + std_ra, ls=':', alpha=0.5, zorder=5, color='r')
-    ax3.axhline(med_dec + std_dec, ls=':', alpha=0.5, zorder=5, color='r')
-    ax1.axvline(med_ra - std_ra, ls=':', alpha=0.5, zorder=5, color='r')
-    ax1.axhline(med_dec - std_dec, ls=':', alpha=0.5, zorder=5, color='r')
-    ax2.axvline(med_ra - std_ra, ls=':', alpha=0.5, zorder=5, color='r')
-    ax3.axhline(med_dec - std_dec, ls=':', alpha=0.5, zorder=5, color='r')
+    stdline = offset_ax.axvline(med_ra + std_ra, ls=':', alpha=0.5, zorder=5, color='r')
+    offset_ax.axhline(med_dec + std_dec, ls=':', alpha=0.5, zorder=5, color='r')
+    ra_ax.axvline(med_ra + std_ra, ls=':', alpha=0.5, zorder=5, color='r')
+    dec_ax.axhline(med_dec + std_dec, ls=':', alpha=0.5, zorder=5, color='r')
+    offset_ax.axvline(med_ra - std_ra, ls=':', alpha=0.5, zorder=5, color='r')
+    offset_ax.axhline(med_dec - std_dec, ls=':', alpha=0.5, zorder=5, color='r')
+    ra_ax.axvline(med_ra - std_ra, ls=':', alpha=0.5, zorder=5, color='r')
+    dec_ax.axhline(med_dec - std_dec, ls=':', alpha=0.5, zorder=5, color='r')
     
     logger.info(f'Matching to {len(all_epochs)} {survey.upper()} measurements with SNR > {snrlim}')
     unique = all_epochs.drop_duplicates(subset=[f'{survey}_ra', f'{survey}_dec'])
@@ -135,26 +136,28 @@ def main(axlim, alpha, bins, matchdir, snrlim, regions, survey, verbose):
     logger.info(f"Right ascension offset of {med_ra:.2f} +/- {std_ra:.2f} arcsec")
     logger.info(f"and standard error of {stderr_ra:.4f} arcsec")
     logger.info(f"Declination offset of {med_dec:.2f} +/- {std_dec:.2f} arcsec")
-    logger.info(f"    and standard error of {stderr_dec:.4f} arcsec")
+    logger.info(f"and standard error of {stderr_dec:.4f} arcsec")
 
-    ax1.set_xlim([-(axlim + .2), axlim + .2])
-    ax1.set_ylim([-(axlim + .2), axlim + .2])
-    ax2.set_xlim([-(axlim + .2), axlim + .2])
-    ax3.set_ylim([-(axlim + .2), axlim + .2])
+    offset_ax.set_xlim([-(axlim + .2), axlim + .2])
+    offset_ax.set_ylim([-(axlim + .2), axlim + .2])
+    ra_ax.set_xlim([-(axlim + .2), axlim + .2])
+    dec_ax.set_ylim([-(axlim + .2), axlim + .2])
 
-    ax2.set_xticks(range(-axlim, axlim+1))
-    ax3.set_yticks(range(-axlim, axlim+1))
-    ax1.set_xticks(range(-axlim, axlim+1))
-    ax1.set_yticks(range(-axlim, axlim+1))
+    ra_ax.set_xticks(range(-axlim, axlim+1))
+    dec_ax.set_yticks(range(-axlim, axlim+1))
+    offset_ax.set_xticks(range(-axlim, axlim+1))
+    offset_ax.set_yticks(range(-axlim, axlim+1))
     
-    leg1 = ax1.legend(handles=handles, labels=labels, loc=3, ncol=4)
-    leg2 = ax1.legend(handles=[medline, stdline, pixbox],
-                      labels=['median', 'stddev', 'single pixel'],
-                      loc=1)
-    ax1.add_artist(leg1)
-    ax1.add_artist(leg2)
+    leg1 = offset_ax.legend(handles=handles, labels=labels, loc=3, ncol=4)
+    leg2 = offset_ax.legend(handles=[medline, stdline, pixbox],
+                            labels=['median', 'stddev', 'single pixel'],
+                            loc=1)
+    offset_ax.add_artist(leg1)
+    offset_ax.add_artist(leg2)
     
-    # fig.savefig('vastp1_astrometry.png', dpi=300, bbox_inches='tight')
+    if save:
+        fig.savefig(f'{save}.png', dpi=300, format='png', bbox_inches='tight')
+
     plt.show()
 
 if __name__ == '__main__':
